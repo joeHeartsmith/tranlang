cfe3dee08e0e49d42e9ee024437ec3454c5a4641
Joe Jamison <mail@joeJamison.net>
2021-12-11 15:39:23 -0600

    Update test-module.py
    Replace regex href parser w/actual HTML parser (Python html.parser).  Also check for code blocks in text, and flag all other text with 'TRANSLATE!' flag to be passed to API later.

--- test-module.py
+++ test-module.py
@@ -1,26 +1,70 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-# Test CGI prototype module.  Presents content + updates hyperlinks.
+# Updated prototype.  Uses actual HTML parsing, and exempts code blocks from translation.  Still only updates hyperlinks.
 
-import os, re
+import os
+from html.parser import HTMLParser
 
-pathprefix = "/var/www/html/quickstart/public/"
-content = pathprefix + str(os.environ.get('QUERY_STRING')).split("page=")[1]
+pathprefix = '/var/www/html/quickstart/public/'
 docroot = 'index.html'
-thisscript = str(os.environ.get('SCRIPT_NAME'))
 pagearg = 'page'
+link_keyword = 'posts'
 
-f = open(content, "r")
-l = f.readlines()
+try:
+    thisscript = str(os.environ.get('SCRIPT_NAME'))
+except:
+    thisscript = 'tranlang.cgi'
+try:
+    content = pathprefix + str(os.environ.get('QUERY_STRING')).split("page=")[1]
+except:
+    content = pathprefix + 'index.html'
+try:
+    f = open(content, 'r')
+except:
+    content = pathprefix + 'index.html'
+    f = open(content, 'r')
 
-print("Content-Type: text/html")
-print()
-for i in l:
-    if "<a href=" in str(i):
-        origlink = re.findall('href="/.+/"|href="/"', i, flags=re.IGNORECASE)[0].lstrip('href=').strip('"') + docroot + '"'
-        print('<a href="' + thisscript + '?' + pagearg + '=' + origlink, i.strip()[len(origlink):], end='')
-    else:
-        print(i, end='')
+class docparser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print('<{}'.format(tag), end='')
+        for attr in attrs:
+            stop = False
+            if tag == str('a'):
+                if attr[0] == str('href'):
+                    if link_keyword in attr[1] or attr[1] == "/":
+                        print(' {}="{}" '.format(attr[0], thisscript + '?' + pagearg + '=' + attr[1] + docroot), end='')
+                        stop = True  # gross. fix this control flow later.
+            if stop != True:
+                print(' {}="{}" '.format(attr[0], attr[1]), end='')
+        print('>', end='')
 
+    def handle_endtag(self, tag):
+        print('</{}>'.format(tag), end='')
+
+    def handle_startendtag(self, tag, attrs):
+        print('<{}'.format(tag), end='')
+        for attr in attrs:
+            print(' {}="{}"'.format(attr[0], attr[1]), end='')
+        print(' />', end='')
+
+    def handle_data(self, data):
+        if len(data.strip()) > 0 and 'XXXPAGECODEFLAGXXX' not in data:
+            print('(TRANSLATE!) {}'.format(data), end='')
+        else:
+            print('{}'.format(data).replace('XXXPAGECODEFLAGXXX', ''), end='')
+
+    def handle_decl(self, data):
+        print('<!{}>'.format(data), end='')
+# TODO: add handle_pi, handle_charref, handle_entityref, and unknown_decl methods
+parser = docparser()
+
+l = f.read()
 f.close()
+
+page_render = l.replace('<code>', '<code> XXXPAGECODEFLAGXXX')
+
+# TODO: Fill HTTP header fields (especially 'Content-Language')
+print('Content-Type: text/html')
 print()
+
+parser.feed(page_render)
